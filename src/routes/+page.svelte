55dd628a01a6003483c8830c93401f1dfe0b2feb
Catus <catus.pl@gmail.com>
2024-04-23 13:57:17 +0200

    Technologies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import About from '$lib/sections/About.svelte';
 	import Projects from '$lib/sections/Projects.svelte';
 	import Contact from '$lib/sections/Contact.svelte';
+	import Technologies from '$lib/sections/Technologies.svelte';
 </script>
 
 <div class="app">
@@ -12,25 +13,8 @@
 	<Hero />
 	<Projects />
 	<About />
-
-	<p>
-		Saepe eum odit maxime adipisci voluptas laboriosam voluptatem doloremque ut laborum magni
-		beatae, at suscipit, mollitia quasi assumenda sit maiores exercitationem culpa ipsum ipsam
-		laudantium nihil quaerat? Recusandae, facere est!
-	</p>
-	<p>
-		Velit vitae tempore est assumenda, quidem dolorum saepe natus perferendis amet iure illo
-		voluptatem recusandae similique consectetur eveniet expedita. Ipsa blanditiis maxime dignissimos
-		ullam saepe. Ducimus fugiat excepturi perspiciatis amet!
-	</p>
-	<p>
-		Accusamus iste magni at numquam commodi voluptatum sequi laboriosam delectus laudantium dolorum
-		quos, dicta quibusdam excepturi deleniti exercitationem quia doloremque, nesciunt ipsam, nisi
-		rerum quaerat? Culpa itaque fugiat neque eligendi.
-	</p>
-
+	<Technologies />
 	<Contact />
-
 	<Footer />
 </div>
 
